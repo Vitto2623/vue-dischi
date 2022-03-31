52d60874a62a95284e5045d5b4cf74cbc99fbe38
Vitto2623 <vtr.1@icloud.com>
2022-03-31 15:09:09 +0200

    commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="container-fluid my-header">
       <div class="row">
-          <div class="col-12">
+          <div class="col-6">
               <img src="https://media.ilsoftware.it/images/500x500/img_19519.jpg" alt="logo-spotify" class="p-2">
+          </div>
+          <div class="col-6 d-flex justify-content-end align-items-center pe-4">
+              <select name="generi-musicali" class="h-50">
+                  <option value="rock">Rock</option>
+                  <option value="pop">Pop</option>
+                  <option value="jazz">Jazz</option>
+                  <option value="metal">Metal</option>
+              </select>
           </div>
       </div>
   </div>
@@ -10,7 +18,7 @@
 
 <script>
 export default {
-
+    name: 'Header',
 }
 </script>
 
